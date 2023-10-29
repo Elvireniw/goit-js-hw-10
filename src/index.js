23a4355e4f943a409915af2c868262b4c96b02b6
Elvireniw <130295282+Elvireniw@users.noreply.github.com>
2023-10-29 08:40:50 +0300

    bugs-commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import { createMarkup, createMarkupCat } from './js/markup';
 import debounce from 'lodash.debounce';
 
-
 const catInfo = document.querySelector('.cat-info');
 const breedSelect = document.querySelector('.breed-select');
-
 const errorMessage = document.querySelector('.error');
 const loaderMessage = document.querySelector('.loader');
+const loaderIcon = document.querySelector('.loader-icon');
 
 const DEBOUNCE_DELAY = 300;
 
@@ -18,17 +17,18 @@ breedSelect.addEventListener('change', debounce(onInputSearch, DEBOUNCE_DELAY));
 fetchBreeds()
   .then(array => {
     breedSelect.innerHTML = '<option value= "" selected disabled>Choose your cat</option> ';
-    breedSelect.classList.toggle('is-hidden')
+    breedSelect.classList.toggle('is-hidden');
+    loaderMessage.classList.add('is-hidden');
+    loaderIcon.classList.add('is-hidden');
     return (breedSelect.innerHTML += createMarkup(array.data));
   })
-//   .then(() => slim())
   .catch(fetchError);
 
 function onInputSearch(e) {
   e.preventDefault();
 
-  catInfo.classList.add('is-hidden')
-  load()
+  catInfo.classList.add('is-hidden');
+  load();
 
   const catID = e.target.value;
 
@@ -41,16 +41,21 @@ function onInputSearch(e) {
 };
 
 function fetchError() {
-  errorMessage.classList.remove('is-hidden')
+  loaderMessage.classList.add('is-hidden');
+  catInfo.classList.add('is-hidden');
+  loaderIcon.classList.add('is-hidden');
+  errorMessage.classList.remove('is-hidden');
 }
 function fetchSuccess() {
   Notify.success('Success!', '');
-  catInfo.classList.remove('is-hidden')
-  loaderMessage.classList.add('is-hidden')
-  console.log('stop loading')
+  errorMessage.classList.add('is-hidden');
+
+  catInfo.classList.remove('is-hidden');
+  loaderMessage.classList.add('is-hidden');
+  console.log('stop loading');
 }
 function load() {
-  console.log('loading...')
-  loaderMessage.classList.remove('is-hidden')
+  console.log('loading...');
+  errorMessage.classList.add('is-hidden');
+  loaderMessage.classList.remove('is-hidden');
 }
-
